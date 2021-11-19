f55c570f064c8ef49c96dac51dac52b10b08f436
Ammar Zakwan <79646199+ammarzakwan-cell@users.noreply.github.com>
2021-11-19 23:29:10 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var apple = ['Apple'];
+  var orange = ['Orange'];
+  var rambutan = ['Rambutan'];
+  var _number;
+  var _message = '';
+
+  final numCon = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter List'),
+        ),
+        body: Column(children: [
+          Text('The list of fruits:'),
+          Text('1) ' + apple[0]),
+          Text('2) ' + orange[0]),
+          Text('3) ' + rambutan[0]),
+          TextField(
+              controller: numCon,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: "Enter fruit number",
+              )),
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                FunctionMessage();
+              });
+            },
+            child: Text('Submit'),
+          ),
+          Text('You fruit is ' + _message),
+        ]),
+      ),
+    );
+  }
+
+  void FunctionMessage() {
+    int value = int.parse(numCon.text);
+    var fruitName;
+    if (value == 1) {
+      fruitName = 'Apple';
+    } else if (value == 2) {
+      fruitName = 'Orange';
+    } else {
+      fruitName = 'Rambutan';
+    }
+
+    setState(() => _message = fruitName);
+  }
+}
